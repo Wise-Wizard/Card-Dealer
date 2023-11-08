2d09d1f1a38c07af4e22914d178102cecd2b182f
Wise-Wizard <saransh.shankar@gmail.com>
2023-11-09 00:02:20 +0530

    Testing

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,24 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+}
 
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktest")
+	deck := newDeck()
+	deck.saveToFile("_decktest")
+	loadedDeck := newDeckFromFile("_decktest")
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(loadedDeck))
+	}
+	os.Remove("_decktest")
 }
